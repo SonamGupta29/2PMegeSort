6288e6acb9f52ea4b8c5ec448482fafdcdc60aa9
buggynap <ganesh.borle@gmail.com>
2016-02-10 19:49:38 +0530

    merging done

--- src/Init.java
+++ src/Init.java
@@ -30,63 +30,81 @@ public class Init
 	public static long BLOCK_SIZE = 0;
 	public static Vector <String> tableColNames;
 	public static Vector <String> tableColDataTypes;
-	
+
+	public static void main(String[] args) 
+	{
+		//Calculate the execution time
+		long startTime = System.currentTimeMillis();
+
+		//Parse the input
+		parseInput(args);
+
+		//Read meta file in hash
+		readMetaFile();
+
+		//Begin sorting
+		beginSort();	
+
+		long endTime = System.currentTimeMillis();
+		System.out.println("Execution Time : " + (endTime - startTime)/1000 +" sec.");
+	}
+
 	private static void parseInput(String[] args) {
-		
+
 		/*	Parse the command line arguments here and make them static 
 		 * 	so that they can be used anywhere in the program  
 		 */
-		
+
 		/*	Command line format : 
 		 *  "./mysort --­­meta_file metafile.txt --­­input_file input.txt ­­--output_file output.txt --­­output_column 
 		 *	col1,col2 --­­sort_column col1,col2 --­­mm 1040 ­­--order asc"		  
 		 */
 
 		int argLength = args.length;
-		
+
 		//Check for the count
 		if(argLength != 14) {
-			 System.err.println("Error!!! Invalid arguments.");
-			 System.err.println("Usage : /n\"Init --meta_file <fileName> --input_file <fileName> ­--output_file <fileName> " +
-			 					 "­--output_column <col1,col2,...> --sort_column <col1,col2,...> --mm <size in MB> --order <asc/desc>\"");
-			 System.exit(0);
+			System.err.println("Error!!! Invalid arguments.");
+			System.err.println("Usage : /n\"Init --meta_file <fileName> --input_file <fileName> ­--output_file <fileName> " +
+					"­--output_column <col1,col2,...> --sort_column <col1,col2,...> --mm <size in MB> --order <asc/desc>\"");
+			System.exit(0);
 		}
-		
+
 		//Extract the command line switches
 		for(int i = 0; i < argLength; i++) {
 			switch(args[i].trim()) {
 				case "--meta_file":
-						metaFile = args[++i].trim();
-						break;
+					metaFile = args[++i].trim();
+					break;
 				case "--input_file":
-						inputFile = args[++i].trim();
-						break;
+					inputFile = args[++i].trim();
+					break;
 				case "--output_file":
-						outputFile = args[++i].trim();
-						break;
+					outputFile = args[++i].trim();
+					break;
 				case "--output_column":
-						String tempList[] = args[++i].trim().split(",");
-						outputColumnList = new Vector <String>();
-						for(int j = 0; j < tempList.length; j++)
-							outputColumnList.add(tempList[j]);
-						break;
+					String tempList[] = args[++i].trim().split(",");
+					outputColumnList = new Vector <String>();
+					for(int j = 0; j < tempList.length; j++)
+						outputColumnList.add(tempList[j]);
+					break;
 				case "--sort_column":
-						String tempList2[] = args[++i].trim().split(",");
-						sortColumnList = new Vector <String>();
-						for(int j = 0; j < tempList2.length; j++)
-							sortColumnList.add(tempList2[j]);
-						break;
+					String tempList2[] = args[++i].trim().split(",");
+					sortColumnList = new Vector <String>();
+					for(int j = 0; j < tempList2.length; j++)
+						sortColumnList.add(tempList2[j]);
+					break;
 				case "--mm":
-						mainMemorySize = Integer.parseInt(args[++i].trim());
-						break;
+					mainMemorySize = Integer.parseInt(args[++i].trim());
+					break;
 				case "--order":
-						sortOrder = args[++i];
-						break;
+					sortOrder = args[++i];
+					break;
 				default:
-						System.err.println("Error in arguments...");
-						System.exit(0);
-						break;
-			}
+					System.err.println("Error in arguments...");
+					System.exit(0);
+					break;
+				}
 		}
 		System.out.println("metaFileName : " + metaFile );
 		System.out.println("inputFile : " + inputFile );
@@ -96,7 +114,7 @@ public class Init
 		System.out.println("mainMemorySize : " + mainMemorySize );
 		System.out.println("sortOrder : " + sortOrder );
 	}
-	
+
 	public static void readMetaFile()
 	{
 		File f = new File(metaFile);
@@ -142,16 +160,16 @@ public class Init
 		}
 		//Loop will add size for one extra comma after EOL, substract it
 		sizeOfRecord -= 1;
-		
+
 		//Consider the new line character at the end of line
 		sizeOfRecord += 1;
-		
+
 		//Get the total number of records that can be fit in a main memory
 		BLOCK_SIZE = (mainMemorySize*1024*1024)/sizeOfRecord;
-		
+
 		System.out.println("RecordSize : " + sizeOfRecord +"B");
 		System.out.println("Block size : " + BLOCK_SIZE);
-	
+
 	}
 
 	private static void beginSort()
@@ -164,7 +182,7 @@ public class Init
 		 * 	start reading the file and once we reach the "Total records that can be fit into the main memory"
 		 * 	sort that data and create a file for that
 		 */
-		
+
 		long noOfRecordsInFile = 0;
 		BufferedReader bfr = null;
 		File f = null;
@@ -172,30 +190,30 @@ public class Init
 		int lineCounter = 0, blockCounter = 0;
 		ArrayList <String> blockList = null;
 		List<File> intermediateFileList = null;
-		
+
 		f = new File(inputFile);
-		
+
 		System.out.println(inputFile + " : " + f.length());
 		noOfRecordsInFile = f.length() / sizeOfRecord;
 		System.out.println("Number of records in file : " + noOfRecordsInFile + "(on average)");
-		
+
 		if(noOfRecordsInFile / BLOCK_SIZE > BLOCK_SIZE  - 1) {
 			System.err.println("Input file is large. Can cause 3 phase merge sort\nExiting...");
 			System.exit(0);
 		}
-		
+
 		System.out.println("Total intermediate files that can be made : " + (f.length() / sizeOfRecord) / BLOCK_SIZE);
-		
+
 		try {
 			bfr = new BufferedReader(new FileReader(new File(inputFile)));
 			//Read the records till the BLOCK SIZE - 1
 			blockList = new ArrayList<>();
 			intermediateFileList = new ArrayList<>();
-			while((line = bfr.readLine()) != null ){				
-				if(lineCounter == BLOCK_SIZE - 1){
+			while((line = bfr.readLine()) != null ) {
+				if(lineCounter == BLOCK_SIZE - 1) {
 					blockCounter++;
 					intermediateFileList.add(new File(sortNCreateTempFile(blockList, blockCounter)));
-					
+
 					//Reset the variable
 					lineCounter = 0;
 					blockList.clear();
@@ -204,7 +222,7 @@ public class Init
 				lineCounter++;
 			}
 			//Check if final lineCounter value is less than the BLOCK SIZE, then create the file for the remaining data 
-			if(lineCounter < BLOCK_SIZE - 1){
+			if(lineCounter < BLOCK_SIZE - 1) {
 				blockCounter++;
 				intermediateFileList.add(new File(sortNCreateTempFile(blockList, blockCounter)));
 				lineCounter = 0;
@@ -213,13 +231,13 @@ public class Init
 			System.err.println("\""+inputFile+"\" not found	.");
 			System.exit(0);	
 		}
-		
+
 		//Printing the file list
 		System.out.println("Temporary file list : ");
-		for(int i = 0; i < blockCounter; i++){
+		for(int i = 0; i < blockCounter; i++) {
 			System.out.println(intermediateFileList.get(i));
 		}
-		
+
 		//Sort and merge the intermediate files
 		try {
 			System.out.println(mergeSortedFiles(intermediateFileList, new File(outputFile)));
@@ -227,14 +245,14 @@ public class Init
 			e.printStackTrace();
 		}		
 	}
-	
+
 	public static String sortNCreateTempFile(ArrayList<String> blockList, int fileCount) {
-		
+
 		String fileName = "i_".concat(String.valueOf(fileCount));
 		Collections.sort(blockList, new Comparator<String>(){
 
 			@Override
-			public int compare(String s1, String s2){
+			public int compare(String s1, String s2) {
 				int indexToSort = 0;
 				for(int i = 0; i < getSortColumnListSize(); i++) {
 					indexToSort = getIndexOfSortColumn(getSortColumnName(i));
@@ -243,33 +261,33 @@ public class Init
 					}
 				}
 				//System.out.println(indexToSort);
-				if(getDataTypeOfColumn(indexToSort).contains("int")){
-						if(Integer.parseInt(s1.split(",")[indexToSort]) < Integer.parseInt(s2.split(",")[indexToSort]))
-							return (sortOrder.equals("desc")? 1 : -1);
-						else if(Integer.parseInt(s1.split(",")[indexToSort]) == Integer.parseInt(s2.split(",")[indexToSort]))
-							return 0;
-						else
-							return (sortOrder.equals("desc")? -1 : 1);
+				if(getDataTypeOfColumn(indexToSort).contains("int")) {
+					if(Integer.parseInt(s1.split(",")[indexToSort]) < Integer.parseInt(s2.split(",")[indexToSort]))
+						return (sortOrder.equals("desc")? 1 : -1);
+					else if(Integer.parseInt(s1.split(",")[indexToSort]) == Integer.parseInt(s2.split(",")[indexToSort]))
+						return 0;
+					else
+						return (sortOrder.equals("desc")? -1 : 1);
 				} else if(getDataTypeOfColumn(indexToSort).contains("char")) {
-						return s1.split(",")[indexToSort].compareTo(s2.split(",")[indexToSort]);
+					return s1.split(",")[indexToSort].compareTo(s2.split(",")[indexToSort]);
 				} else if(getDataTypeOfColumn(indexToSort).contains("date")) {
-						DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
-						try {
-							int ret = f.parse(s1.split(",")[indexToSort]).compareTo(f.parse(s2.split(",")[indexToSort]));
-							if(ret == -1)
-								return (sortOrder.equals("desc")? 1 : -1);
-							else if(ret == 1)
-								return (sortOrder.equals("desc")? -1 : 1);
-							else
-								return 0;
-						} catch (ParseException e1) {
-							e1.printStackTrace();
-						}
+					DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+					try {
+						int ret = f.parse(s1.split(",")[indexToSort]).compareTo(f.parse(s2.split(",")[indexToSort]));
+						if(ret == -1)
+							return (sortOrder.equals("desc")? 1 : -1);
+						else if(ret == 1)
+							return (sortOrder.equals("desc")? -1 : 1);
+						else
+							return 0;
+					} catch (ParseException e1) {
+						e1.printStackTrace();
+					}
 				}
 				return 0;
 			}	
 		});
-				
+
 		FileWriter fwr = null;
 		//create a intermediate file
 		try {
@@ -283,19 +301,19 @@ public class Init
 		}		
 		return fileName;
 	}
-	
+
 	public static int getSortColumnListSize(){
-		
+
 		return sortColumnList.size();
 	}
-	
+
 	public static String getSortColumnName(int index){
-		
+
 		return sortColumnList.get(index);
 	}
-	
+
 	public static int getIndexOfSortColumn(String colName){
-		
+
 		int index = 0;
 		for(index = 0; index < tableColNames.size(); index++) {
 			if(tableColNames.get(index).equals(colName)) {
@@ -303,66 +321,67 @@ public class Init
 			}
 		}
 		return -1;
-			
+
 	}
 	public static String getDataTypeOfColumn(int index){
-		
+
 		return tableColDataTypes.get(index);
 	}
-	
+
 	public static long getBlockSize(){
-		
+
 		return BLOCK_SIZE;
-	}
-	
-	
+	}	
+
 	public static int mergeSortedFiles(List<File> files, File outputfile) throws IOException {
-		
+
 		PriorityQueue<BinaryFileBuffer> pq = new PriorityQueue<BinaryFileBuffer>((int)getBlockSize() - 1, 
 				new Comparator<BinaryFileBuffer>() {
-					public int compare(BinaryFileBuffer i, BinaryFileBuffer j) {
-								String s1 =	i.peek();  
-								String s2 = j.peek();
-									int indexToSort = 0;
-									for(int iterator = 0; iterator < getSortColumnListSize(); iterator++) {
-										indexToSort = getIndexOfSortColumn(getSortColumnName(iterator));
-										if(!s1.split(",")[indexToSort].equals(s2.split(",")[indexToSort])) {
-											break;
-										}
-									}
-									if(getDataTypeOfColumn(indexToSort).contains("int")){
-											if(Integer.parseInt(s1.split(",")[indexToSort]) < Integer.parseInt(s2.split(",")[indexToSort]))
-												return (sortOrder.equals("desc")? 1 : -1);
-											else if(Integer.parseInt(s1.split(",")[indexToSort]) == Integer.parseInt(s2.split(",")[indexToSort]))
-												return 0;
-											else
-												return (sortOrder.equals("desc")? -1 : 1);
-									} else if(getDataTypeOfColumn(indexToSort).contains("char")) {
-											return s1.split(",")[indexToSort].compareTo(s2.split(",")[indexToSort]);
-									} else if(getDataTypeOfColumn(indexToSort).contains("date")) {
-											DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
-											try {
-												int ret = f.parse(s1.split(",")[indexToSort]).compareTo(f.parse(s2.split(",")[indexToSort]));
-												if(ret == -1)
-													return (sortOrder.equals("desc")? 1 : -1);
-												else if(ret == 1)
-													return (sortOrder.equals("desc")? -1 : 1);
-												else
-													return 0;
-											} catch (ParseException e1) {
-												e1.printStackTrace();
-											}
-									}
-									return 0;
-								}	
-							});
-		
+
+			@Override
+			public int compare(BinaryFileBuffer i, BinaryFileBuffer j) {
+				String s1 =	i.peek();  
+				String s2 = j.peek();
+				int indexToSort = 0;
+				for(int iterator = 0; iterator < getSortColumnListSize(); iterator++) {
+					indexToSort = getIndexOfSortColumn(getSortColumnName(iterator));
+					if(!s1.split(",")[indexToSort].equals(s2.split(",")[indexToSort])) {
+						break;
+					}
+				}
+				if(getDataTypeOfColumn(indexToSort).contains("int")){
+					if(Integer.parseInt(s1.split(",")[indexToSort]) < Integer.parseInt(s2.split(",")[indexToSort]))
+						return (sortOrder.equals("desc")? 1 : -1);
+					else if(Integer.parseInt(s1.split(",")[indexToSort]) == Integer.parseInt(s2.split(",")[indexToSort]))
+						return 0;
+					else
+						return (sortOrder.equals("desc")? -1 : 1);
+				} else if(getDataTypeOfColumn(indexToSort).contains("char")) {
+					return s1.split(",")[indexToSort].compareTo(s2.split(",")[indexToSort]);
+				} else if(getDataTypeOfColumn(indexToSort).contains("date")) {
+					DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+					try {
+						int ret = f.parse(s1.split(",")[indexToSort]).compareTo(f.parse(s2.split(",")[indexToSort]));
+						if(ret == -1)
+							return (sortOrder.equals("desc")? 1 : -1);
+						else if(ret == 1)
+							return (sortOrder.equals("desc")? -1 : 1);
+						else
+							return 0;
+					} catch (ParseException e1) {
+						e1.printStackTrace();
+					}
+				}
+				return 0;
+			}	
+		});
+
 		for (File f : files) {
-			BinaryFileBuffer bfb = new BinaryFileBuffer(f);
-			pq.add(bfb);
+			pq.add(new BinaryFileBuffer(f));
 		}
 		BufferedWriter fbw = new BufferedWriter(new FileWriter(outputfile));
 		int rowcounter = 0;
+
 		try {
 			while(pq.size() > 0) {
 				BinaryFileBuffer bfb = pq.poll();
@@ -375,33 +394,15 @@ public class Init
 					bfb.originalfile.delete();// we don't need you anymore
 				} else {
 					pq.add(bfb); // add it back
-		        }
-		    }
+				}
+			}
 		} catch(Exception e) {
-			System.out.println(e.printStackTrace());
+			//System.out.println(e.printStackTrace());
 		} finally { 
 			fbw.close();
 			for(BinaryFileBuffer bfb : pq ) 
 				bfb.close();
 		}
 		return rowcounter;
-	}
-	
-	public static void main(String[] args) 
-	{
-		//Calculate the execution time
-		long startTime = System.currentTimeMillis();
-		
-		//Parse the input
-		parseInput(args);
-		
-		//Read meta file in hash
-		readMetaFile();
-		
-		//Begin sorting
-		beginSort();	
-		
-		long endTime = System.currentTimeMillis();
-		System.out.println("Execution Time : " + (endTime - startTime)/1000 +" sec.");
-	}
+	}	
 }
